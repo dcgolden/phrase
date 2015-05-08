99fd85f22e94db6af9baebf4eae31e860cf999b7
Alex <alexrules313@gmail.com>
2015-05-08 10:54:50 -0400

    big fixin

--- bivitfrontend/app/scripts/app.js
+++ bivitfrontend/app/scripts/app.js
@@ -15,7 +15,8 @@ angular
     'ngResource',
     'ngRoute',
     'ngSanitize',
-    'ngTouch'
+    'ngTouch',
+    'ngMaterial'
   ])
 
   .config(function ($routeProvider) {
@@ -99,24 +100,19 @@ angular
       .otherwise({
         redirectTo: 'views/404.html'
       });
-  });
+  },
 
-angular
-  .module('bivitfrontSampleApp', [
-    'ngMaterial'
-  ])
-
-  .config(function ($mdThemingProvider) {
+  function ($mdThemingProvider) {
   $mdThemingProvider.theme('default')
     .primaryPalette('cyan', {
       'default': '500', // by default use shade 400 from the pink palette for primary intentions
       'hue-1': '100', // use shade 100 for the <code>md-hue-1</code> class
-      'hue-2': '500', // use shade 600 for the <code>md-hue-2</code> class
-      'hue-3': '700' // use shade A100 for the <code>md-hue-3</code> class
+      'hue-2': '500', // use shade 500 for the <code>md-hue-2</code> class
+      'hue-3': '700' // use shade 700 for the <code>md-hue-3</code> class
     })
     // If you specify less than all of the keys, it will inherit from the
     // default shades
     .accentPalette('deep-orange', {
-      'default': 'A200' // use shade 200 for default, and keep all other shades the same
+      'default': 'A200' // use shade A200 for default, and keep all other shades the same
     });
   });
