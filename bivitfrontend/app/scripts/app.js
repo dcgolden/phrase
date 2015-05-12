b7fb82974595f5d1e4ff158139c211dfc9e9a46e
Alex <alexrules313@gmail.com>
2015-05-12 10:42:14 -0400

    attempted to implement icons

--- bivitfrontend/app/scripts/app.js
+++ bivitfrontend/app/scripts/app.js
@@ -16,7 +16,8 @@ angular
     'ngRoute',
     'ngSanitize',
     'ngTouch',
-    'ngMaterial'
+    'ngMaterial',
+    'ngMdIcons'
   ])
 
   .controller('NavCtrl', function ($scope, $timeout, $mdSidenav, $mdUtil, $log) {
