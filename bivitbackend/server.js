104b13244f39f4eb8a79e98c36b507328761d02f
David Golden <dcgolden1@gmail.com>
2015-05-12 09:52:55 -0400

    stuff

--- bivitbackend/server.js
+++ bivitbackend/server.js
@@ -4,6 +4,7 @@
 // call the packages we need
 var express    = require('express'); 		// call express
 var app        = express(); 				// define our app using express
+var unirest = require('unirest');
 var bodyParser = require('body-parser');
 var mongoose   = require('mongoose');
 var passport = require('passport');
--- bivitfrontend/app/scripts/controllers/articlesAdd.js
+++ bivitfrontend/app/scripts/controllers/articlesAdd.js
@@ -1,5 +1,4 @@
 'use strict';
-
 /**
  * @ngdoc function
  * @name bivitfrontSampleApp.controller:ArticlesAddCtrl
@@ -9,5 +8,28 @@
  */
 angular.module('bivitfrontSampleApp')
   .controller('ArticlesAddCtrl', function ($scope, $http, $route, $routeParams) {
-});
+  	var headers = {
+  		'Content-Type': 'application/x-www-form-urlencoded'
+	};
+	var options = 
+	{
+		host: 'http://localhost',
+		port: '8080',
+		path: '/api/articles',
+		method: 'POST',
+		headers: headers
+	};
 
+	var article = {
+		content: "$scope.submissionContent", 
+		source:"$scope.submissionSource", 
+		author:"$scope.submissionAuthor", 
+		title:"$scope.submissionTitle"};
+
+	var request = $http.request(options, article);
+  	
+  	$scope.addArticle = function (data)
+  	{
+  		request();
+  	}
+});
