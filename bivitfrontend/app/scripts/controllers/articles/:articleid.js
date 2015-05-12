e41010158743c3daf9b172c2fd339c8758906c21
David Golden <dcgolden1@gmail.com>
2015-05-12 14:20:05 -0400

    changes

--- bivitfrontend/app/scripts/controllers/articles/:articleid.js
+++ bivitfrontend/app/scripts/controllers/articles/:articleid.js
@@ -8,32 +8,17 @@
  * Controller of the bivitfrontSampleApp
  */
 angular.module('bivitfrontSampleApp')
-  .controller('ArticlesArticleidCtrl', function ($scope, $http, $route, $routeParams, $mdDialog) {
-  	var url = 'http://localhost:8080/api/articles/' + $routeParams.articleId;
+    .controller('ArticlesArticleidCtrl', function($scope, $http, $route, $routeParams, $mdDialog) {
 
-  	$http.get(url)
-    	.success( function (data) {
-    		$scope.article = data;
-    		console.log(data);
-    	})
-   		
-      $scope.showConfirm = function(ev) {
-       var confirm = $mdDialog.confirm()
-        .title("Are you sure you want to delete this article and all of it's discussions")
-        .content('This operation is non-reversible')
-        .ariaLabel('Lucky day')
-        .ok('Delete')
-        .cancel('Take me back!')
-        .targetEvent(ev);
-    $mdDialog.show(confirm).then(function() {
-      $scope.deleteArticle = function (data) {
-        $http.delete(url);
-        console.log(data);
-      }
-    }, 
-    function() {
-      
+        var url = 'http://localhost:8080/api/articles/' + $routeParams.articleId;
+
+        $http.get(url)
+            .success(function(data) {
+                $scope.article = data;
+                console.log(data);
+            });
+        $scope.deleteArticle = function(data) {
+            $http.delete(url);
+            console.log(data);
+        };
     });
-  }
-});
-
