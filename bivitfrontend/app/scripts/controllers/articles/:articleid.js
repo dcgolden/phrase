f6e0ddcba7e6c6cc12ee2ed0380bd800699be6bc
Alex <alexrules313@gmail.com>
2015-05-13 09:32:33 -0400

    delete button now redirects and confirm delete

--- bivitfrontend/app/scripts/controllers/articles/:articleid.js
+++ bivitfrontend/app/scripts/controllers/articles/:articleid.js
@@ -8,7 +8,7 @@
  * Controller of the bivitfrontSampleApp
  */
 angular.module('bivitfrontSampleApp')
-    .controller('ArticlesArticleidCtrl', function($scope, $http, $route, $routeParams, $mdDialog) {
+    .controller('ArticlesArticleidCtrl', function($scope, $http, $route, $routeParams, $mdDialog, $location) {
 
         var url = 'http://localhost:8080/api/articles/' + $routeParams.articleId;
 
@@ -17,8 +17,30 @@ angular.module('bivitfrontSampleApp')
                 $scope.article = data;
                 console.log(data);
             });
-        $scope.deleteArticle = function(data) {
+        $scope.deleteArticle = function(path,data) {
             $http.delete(url);
             console.log(data);
+            $location.path( '/articles' );
+        };
+
+        $scope.alert = '';
+
+        $scope.showConfirm = function(ev) {
+            // Appending dialog to document.body to cover sidenav in docs app
+            var confirm = $mdDialog.confirm()
+              .parent(angular.element(document.body))
+              .title('Would you like to delete this article?')
+              .content('This will delete it forever.')
+              .ariaLabel('Article Delete')
+              .ok('Delete')
+              .cancel('Cancel')
+              .targetEvent(ev);
+            $mdDialog.show(confirm).then(function() {
+                $http.delete(url);
+                console.log('deleted!');
+                $location.path( '/articles' );
+            }, function() {
+              $scope.alert = '';
+            });
         };
     });
