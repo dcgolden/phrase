771d5500f8bab6adc14a76b262bbc089fdc65c46
Alex <alexrules313@gmail.com>
2015-05-28 10:20:05 -0400

    fixed the no-error error

--- tnw/app/index.js
+++ tnw/app/index.js
@@ -19,6 +19,22 @@ require('angular').module('app', [
     .state('home', require('./components/home'))
     .state('about', require('./components/about'))
     .state('contact', require('./components/contact'))
+
+  /* THEMEING NOT WORKING 'Cannot read property 'theme' of undefined'
+  $mdThemingProvider.theme('default')
+  .primaryPalette('indigo', {
+    'default': '500', // by default use shade 500 from the palette for primary intentions
+    'hue-1': '100', // use shade 100 for the <code>md-hue-1</code> class
+    'hue-2': '500', // use shade 500 for the <code>md-hue-2</code> class
+    'hue-3': '700' // use shade 700 for the <code>md-hue-3</code> class
+  })
+  // If you specify less than all of the keys, it will inherit from the
+  // default shades
+  .accentPalette('light-blue', {
+    'default': 'A200' // use shade A200 for default, and keep all other shades the same
+  });
+  */
+
 }])
 .directive('appNav', ['users', require('./components/app-nav')])
 .controller('AppController', ['$scope', 'users', AppController])
@@ -26,24 +42,7 @@ require('angular').module('app', [
 .constant('remoteUserDbName', 'https://bivit-dev.iriscouch.com/_users')
 //.factory('classrooms', ['pouchDb', require('./services/classrooms')])
 
-function AppController ($scope, users, $mdThemingProvider, $mdSidenav, $mdUtil, $log) {
-  $scope.showMenu = true
-  $scope.toggle = function () {
-    $scope.showMenu = !$scope.showMenu
-  };
-
-  $mdThemingProvider.theme('default')
-    .primaryPalette('indigo', {
-      'default': '500', // by default use shade 500 from the palette for primary intentions
-      'hue-1': '100', // use shade 100 for the <code>md-hue-1</code> class
-      'hue-2': '500', // use shade 500 for the <code>md-hue-2</code> class
-      'hue-3': '700' // use shade 700 for the <code>md-hue-3</code> class
-    })
-    // If you specify less than all of the keys, it will inherit from the
-    // default shades
-    .accentPalette('light-blue', {
-      'default': 'A200' // use shade A200 for default, and keep all other shades the same
-    });
+function AppController ($scope, users, $mdSidenav, $mdUtil, $log) {
 
   function buildToggler(navID) {
     var debounceFn =  $mdUtil.debounce(function(){
