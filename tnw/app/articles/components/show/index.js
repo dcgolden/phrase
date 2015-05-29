523bec8d9637284eb0d7efc53ed81822121ceffc
Alex <alexrules313@gmail.com>
2015-05-29 11:42:23 -0400

    fixed davids mistakes again again again

--- tnw/app/articles/components/show/index.js
+++ tnw/app/articles/components/show/index.js
@@ -1,40 +1,25 @@
 'use strict';
 
-var fs = require('fs');
+var fs = require('fs')
 
 module.exports = {
-    url: '/:id',
-    controller: ['$scope', 'articles', '$stateParams', controller],
+    url: '/new',
+    controller: ['$scope', 'articles', '$state', controller],
     template: fs.readFileSync(__dirname + '/template.html', 'utf-8')
-};
+}
 
-<<<<<<< HEAD
-$('a').on('click', function() {
-  $('.wrap, a').toggleClass('active');
+function controller($scope, articles, $state) {
 
-  return false;
-});
-
-function controller ($scope, articles, $stateParams) {
-  articles.get($stateParams.id).then(function(doc) {
-    $scope.article = doc;
-  });
-
-  $scope.remove = function (id) {
-    console.log($stateParams.id);
-    articles.remove($stateParams.id).then(function (res) {
-      $state.go('articles.list');
-=======
-function controller($scope, articles, $stateParams) {
-    articles.get($stateParams.id).then(function(doc) {
-        $scope.article = doc;
->>>>>>> origin/master
-    });
-
-    $scope.remove = function(id) {
-        articles.remove(id).then(function(res) {
-                $state.go('articles.list');
-            }
-        );
-    };
+    $scope.create = function(article) {
+        articles.create(article)
+            .then(function(res) {
+                $state.go('articles.list')
+            })
+    }
+    $scope.loadClassrooms = function() {
+        classrooms.list()
+            .then(function(res) {
+                $scope.classrooms = res;
+            })
+    }
 }
