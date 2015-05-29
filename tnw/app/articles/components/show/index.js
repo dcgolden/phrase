3c6601b916b3124f1dd68611de42872af013a29e
Alex <alexrules313@gmail.com>
2015-05-29 12:07:31 -0400

    got rid of spinner

--- tnw/app/articles/components/show/index.js
+++ tnw/app/articles/components/show/index.js
@@ -1,25 +1,28 @@
 'use strict';
 
-var fs = require('fs')
+var fs = require('fs');
 
 module.exports = {
-    url: '/new',
-    controller: ['$scope', 'articles', '$state', controller],
-    template: fs.readFileSync(__dirname + '/template.html', 'utf-8')
-}
+  url: '/:id',
+  controller: ['$scope', 'articles', '$stateParams', controller],
+  template: fs.readFileSync(__dirname + '/template.html', 'utf-8')
+};
 
-function controller($scope, articles, $state) {
+$('a').on('click', function() {
+  $('.wrap, a').toggleClass('active');
 
-    $scope.create = function(article) {
-        articles.create(article)
-            .then(function(res) {
-                $state.go('articles.list')
-            })
-    }
-    $scope.loadClassrooms = function() {
-        classrooms.list()
-            .then(function(res) {
-                $scope.classrooms = res;
-            })
-    }
+  return false;
+});
+
+function controller ($scope, articles, $stateParams) {
+  articles.get($stateParams.id).then(function(doc) {
+    $scope.article = doc;
+  });
+
+  $scope.remove = function (id) {
+    console.log($stateParams.id);
+    articles.remove($stateParams.id).then(function (res) {
+      $state.go('articles.list');
+    });
+  };
 }
