5965eb22168db9543264939be2c49af7f41d23ec
David Golden <dcgolden1@gmail.com>
2015-05-29 09:34:15 -0400

    remove attempt

--- tnw/app/articles/components/show/index.js
+++ tnw/app/articles/components/show/index.js
@@ -1,15 +1,22 @@
-'use strict'
+'use strict';
 
-var fs = require('fs')
+var fs = require('fs');
 
 module.exports = {
   url: '/:id',
   controller: ['$scope', 'articles', '$stateParams', controller],
   template: fs.readFileSync(__dirname + '/template.html', 'utf-8')
-}
+};
 
 function controller ($scope, articles, $stateParams) {
   articles.get($stateParams.id).then(function(doc) {
-    $scope.article = doc
-  })
+    $scope.article = doc;
+  });
+
+  $scope.remove = function (id) {
+    var passID = $stateParams.id;
+    articles.remove(passID).then(function (res) {
+      $state.go('articles.list');
+    });
+  };
 }
