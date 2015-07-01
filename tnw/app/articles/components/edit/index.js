311f70f122e2d01d4580b599f69ea62ac921fa32
David Golden <dcgolden1@gmail.com>
2015-07-01 09:45:29 -0400

    text area sizing

--- tnw/app/articles/components/edit/index.js
+++ tnw/app/articles/components/edit/index.js
@@ -1,22 +1,22 @@
-'use strict'
+'use strict';
 
-var fs = require('fs')
+var fs = require('fs');
 
 module.exports = {
   url: '/:id/edit',
   controller: ['$scope', 'articles', '$stateParams', '$state', controller],
   template: fs.readFileSync(__dirname + '/template.html', 'utf-8')
-}
+};
 
 function controller ($scope, articles, $stateParams, $state) {
 
   articles.get($stateParams.id).then(function (doc) {
-    $scope.article = doc
-  })
+    $scope.article = doc;
+  });
 
   $scope.update = function (article) {
     articles.update(article).then(function (res) {
-      $state.go('articles.show', { id: article._id })
-    })
-  }
+      $state.go('articles.show', { id: article._id });
+    });
+  };
 }
--- tnw/app/articles/components/new/index.js
+++ tnw/app/articles/components/new/index.js
@@ -3,22 +3,20 @@
 var fs = require('fs');
 module.exports = {
     url: '/new',
-    controller: ['$scope', 'articles', 'classrooms', '$state', controller],
+    controller: ['$scope', 'articles', 'classrooms', '$stateParams', '$state', controller],
     template: fs.readFileSync(__dirname + '/template.html', 'utf-8')
 };
 
-function controller($scope, articles, $state, classrooms) {
+function controller($scope, articles, $state) {
 
-    classrooms.list().then(function(res) {
+    /*classrooms.list().then(function(res) {
         $scope.$apply(function() {
             $scope.classrooms = res;
         });
+    });*/
+    $scope.create = function (article) {
+    articles.create(article).then(function (res) {
+      $state.go('articles.list');
     });
-
-    $scope.create = function(article) {
-        articles.create(article)
-            .then(function(res) {
-                $state.go('articles.list');
-            });
-    };
+  };
 }
