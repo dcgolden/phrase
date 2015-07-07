3d4fb11d3e4a47cd51ef113c0c58ff6efae2aa7b
David Golden <dcgolden1@gmail.com>
2015-07-07 13:04:50 -0400

    classroom view

--- tnw/app/articles/services.js
+++ tnw/app/articles/services.js
@@ -1,43 +1,118 @@
 'use strict';
 
 module.exports = {
-  articles: function (pouchDB, dbName) {
-    var db = pouchDB(dbName);
+    articles: function(pouchDB, dbName) {
+        var db = pouchDB(dbName);
 
-    function list () {
-      return db.allDocs({startkey: 'article', endkey: 'article{}', inclusive_end: true, include_docs: true})
-        .then(function (res) {
-          console.log(res.rows);
-          return res.rows.map(function (r) { return r.doc})
-      });
+        function list() {
+            return db.allDocs({
+                    startkey: 'article',
+                    endkey: 'article{}',
+                    inclusive_end: true,
+                    include_docs: true
+                })
+                .then(function(res) {
+                    console.log(res.rows);
+                    return res.rows.map(function(r) {
+                        return r.doc;
+                    });
+                });
+        }
+
+        function create(article) {
+            article.type = 'article';
+            article._id = 'article-' + (new Date()).toISOString()
+            return db.put(article);
+        }
+
+        function get(id) {
+            return db.get(id);
+        }
+
+        function update(article) {
+            return db.put(article);
+        }
+
+        function remove(id) {
+            return db.get(id).then(function(doc) {
+                return db.remove(doc);
+            });
+        }
+
+        return Object.freeze({
+            list: list,
+            create: create,
+            get: get,
+            update: update,
+            remove: remove
+        });
+        /*annotations: function(pouchDB, annotationDbName) {
+            var db = pouchDB(annotationDbName);
+
+            var ddoc = {
+                _id: '_design/annotator',
+                views: {
+                    annotations: {
+                        map: function(doc) {
+                            if ('uri' in doc && 'ranges' in doc) {
+                                emit(doc.uri, 1);
+                            }
+                        }.toString()
+                    }
+                }
+            };
+
+            function create(annotation) {
+                annotation.id = PouchDB.utils.uuid();
+                annotation._id = annotation.id;
+                return db.post(annotation)
+                    .then(function(resp) {
+                        annotation._rev = resp.rev;
+                        return annotation;
+                    })
+                    .catch(console.log.bind(console));
+            },
+
+
+            function query(queryObj) {
+                queryObj.reduce = false;
+                queryObj.include_docs = true;
+                return db.query('annotator/annotations', queryObj)
+                    .then(function(resp) {
+                        var annotations = [];
+                        for (var i = 0; i < resp.rows.length; i++) {
+                            annotations.push(resp.rows[i].doc);
+                        }
+                        return {
+                            results: annotations,
+                            metadata: {
+                                total: resp.rows.length
+                            }
+                        };
+                    })
+                    .catch(console.log.bind(console));
+            }
+
+            function update(annotation) {
+                return db.put(annotation)
+                    .then(function(resp) {
+                        annotation._rev = resp.rev;
+                        return annotation;
+                    })
+                    .catch(console.log.bind(console));
+            } {
+                return db.put(article);
+            }
+
+            function delete(annotation) {
+                return db.remove(annotation)
+                    .then(function(resp) {
+                        return annotation;
+                    })
+                    .catch(console.log.bind(console));
+            }
+            };
+        }
+    })*/
     }
-
-    function create (article) {
-      article.type = 'article';
-      article._id = 'article-' + (new Date()).toISOString()
-      return db.put(article);
-    }
-
-    function get (id) {
-      return db.get(id);
-    }
-
-    function update (article) {
-      return db.put(article);
-    }
-
-    function remove (id) {
-      return db.get(id).then(function (doc) {
-        return db.remove(doc);
-      });
-    }
-
-    return Object.freeze({
-      list: list,
-      create: create,
-      get: get,
-      update: update,
-      remove: remove
-    });
-  }
 };
--- tnw/app/index.js
+++ tnw/app/index.js
@@ -1,7 +1,7 @@
 'use strict'
 
 var cssify = require('cssify')
- 
+
 cssify.byUrl('//cdn.rawgit.com/angular/bower-material/master/angular-material.css')
 cssify.byUrl('/main.css')
 
@@ -10,46 +10,47 @@ window.PouchDB = require('pouchdb')
 window.PouchDB.plugin(require('pouchdb-authentication'))
 
 require('angular').module('app', [
-  require('angular-animate'),
-  require('angular-aria'),
-  require('angular-material'),
-  require('./ng-icons'),
-  require('angular-ui-router'),
-  require('angular-messages'),
-  require('./classrooms'),
-  require('./articles'),
-  require('./users')
-])
-.config(['$stateProvider', '$urlRouterProvider', '$mdThemingProvider', function ($stateProvider, $urlRouterProvider, $mdThemingProvider) {
-  $urlRouterProvider.otherwise('/')
-  $stateProvider
-    .state('home', require('./components/home'))
-    .state('about', require('./components/about'))
-    .state('contact', require('./components/contact'))
+        require('angular-animate'),
+        require('angular-aria'),
+        require('angular-material'),
+        require('./ng-icons'),
+        require('angular-ui-router'),
+        require('angular-messages'),
+        require('./classrooms'),
+        require('./articles'),
+        require('./users')
+    ])
+    .config(['$stateProvider', '$urlRouterProvider', '$mdThemingProvider', function($stateProvider, $urlRouterProvider, $mdThemingProvider) {
+        $urlRouterProvider.otherwise('/')
+        $stateProvider
+            .state('home', require('./components/home'))
+            .state('about', require('./components/about'))
+            .state('contact', require('./components/contact'))
 
-  $mdThemingProvider.theme('default')
-  .primaryPalette('indigo', {
-    'default': '500', // by default use shade 500 from the palette for primary intentions
-    'hue-1': '100', // use shade 100 for the <code>md-hue-1</code> class
-    'hue-2': '500', // use shade 500 for the <code>md-hue-2</code> class
-    'hue-3': '700' // use shade 700 for the <code>md-hue-3</code> class
-  })
-  // If you specify less than all of the keys, it will inherit from the
-  // default shades
-  .accentPalette('pink', {
-    'default': 'A200' // use shade A200 for default, and keep all other shades the same
-  });
+        $mdThemingProvider.theme('default')
+            .primaryPalette('indigo', {
+                'default': '500', // by default use shade 500 from the palette for primary intentions
+                'hue-1': '100', // use shade 100 for the <code>md-hue-1</code> class
+                'hue-2': '500', // use shade 500 for the <code>md-hue-2</code> class
+                'hue-3': '700' // use shade 700 for the <code>md-hue-3</code> class
+            })
+            // If you specify less than all of the keys, it will inherit from the
+            // default shades
+            .accentPalette('pink', {
+                'default': 'A200' // use shade A200 for default, and keep all other shades the same
+            });
 
-}])
-.directive('appNav', ['users', require('./components/app-nav')])
-.controller('AppController', ['$scope', 'users', AppController])
-.constant('dbName', 'https://bivit-dev.iriscouch.com/bivit')
-.constant('remoteUserDbName', 'https://bivit-dev.iriscouch.com/_users')
-//.factory('classrooms', ['pouchDb', require('./services/classrooms')])
+    }])
+    .directive('appNav', ['users', require('./components/app-nav')])
+    .controller('AppController', ['$scope', 'users', AppController])
+    .constant('dbName', 'https://bivit-dev.iriscouch.com/bivit')
+    .constant('remoteUserDbName', 'https://bivit-dev.iriscouch.com/_users')
+    .constant('annotationDbName', 'https://bivit-dev.iriscouch.com/annotator')
+    //.factory('classrooms', ['pouchDb', require('./services/classrooms')])
 
-function AppController ($scope, users) {
-  $scope.showMenu = true;
-  $scope.toggle = function () {
-    $scope.showMenu = !$scope.showMenu;
-  }
+function AppController($scope, users) {
+    $scope.showMenu = true;
+    $scope.toggle = function() {
+        $scope.showMenu = !$scope.showMenu;
+    }
 }
