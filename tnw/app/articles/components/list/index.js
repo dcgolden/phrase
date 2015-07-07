056ce01fee99e7c8d9a2ca238d80c50ecb15e277
David Golden <dcgolden1@gmail.com>
2015-07-07 14:42:18 -0400

    articles list loading indicator

--- tnw/app/articles/components/list/index.js
+++ tnw/app/articles/components/list/index.js
@@ -3,13 +3,16 @@
 var fs = require('fs')
 
 module.exports = {
-  url: '/list',
-  controller: ['$scope', 'articles', controller],
-  template: fs.readFileSync(__dirname + '/template.html', 'utf-8')
+    url: '/list',
+    controller: ['$scope', 'articles', controller],
+    template: fs.readFileSync(__dirname + '/template.html', 'utf-8')
 }
 
-function controller ($scope, articles) {
-  articles.list().then(function (docs) {
-    $scope.articles = docs
-  })
+function controller($scope, articles) {
+	$scope.loadingArticles = true;
+    articles.list()
+        .then(function(docs) {
+            $scope.articles = docs;
+            $scope.loadingArticles = false;
+        })
 }
