234267bda2e214f1b0bc83a66fd815664669438f
David Golden <dcgolden1@gmail.com>
2015-07-07 13:21:21 -0400

    new structure for new articles

---
+++ tnw/app/articles/components/newURL/index.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var fs = require('fs');
+module.exports = {
+    url: '/new',
+    controller: ['$scope', 'articles', 'classrooms', '$stateParams', '$state', controller],
+    template: fs.readFileSync(__dirname + '/template.html', 'utf-8')
+};
+
+function controller($scope, articles, $state) {
+
+    /*classrooms.list().then(function(res) {
+        $scope.$apply(function() {
+            $scope.classrooms = res;
+        });
+    });*/
+    $scope.create = function (article) {
+    articles.create(article).then(function (res) {
+      $state.go('articles.list');
+    });
+  };
+}
