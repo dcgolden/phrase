1b19d35cf180b40a67872b071921324fa61471cd
Alex <alexrules313@gmail.com>
2015-07-08 14:07:33 -0400

    nav bar changes

--- tnw/app/components/app-nav/index.js
+++ tnw/app/components/app-nav/index.js
@@ -5,12 +5,12 @@ var fs = require('fs')
 module.exports = function (users) {
   return {
     restrict: 'E',
-    controller: ['$scope', 'users', '$state', '$mdSidenav', '$mdUtil', '$log', controller],
+    controller: ['$scope', 'users', '$state', '$mdSidenav', '$mdUtil', '$log', '$mdDialog', controller],
     template: fs.readFileSync(__dirname + '/template.html')
   }
 }
 
-function controller ($scope, users, $state, $mdSidenav, $mdUtil, $log) {
+function controller ($scope, users, $state, $mdSidenav, $mdUtil, $log, $mdDialog) {
 
   function buildToggler(navID) {
     var debounceFn =  $mdUtil.debounce(function(){
@@ -23,6 +23,8 @@ function controller ($scope, users, $state, $mdSidenav, $mdUtil, $log) {
 
     return debounceFn;
   }
+
+
 
   $scope.toggleLeft = buildToggler('left');
 
@@ -53,4 +55,30 @@ function controller ($scope, users, $state, $mdSidenav, $mdUtil, $log) {
       })
     })
   }
+
+  $scope.showAdd = function(ev) {
+    $mdDialog.show({
+        controller: DialogController,
+        template: '<md-dialog aria-label="Mango (Fruit)"> <md-content class="md-padding"> <form name="userForm"> <div layout layout-sm="column"> <md-input-container flex> <label>First Name</label> <input ng-model="user.firstName" placeholder="Placeholder text"> </md-input-container> <md-input-container flex> <label>Last Name</label> <input ng-model="theMax"> </md-input-container> </div> <md-input-container flex> <label>Address</label> <input ng-model="user.address"> </md-input-container> <div layout layout-sm="column"> <md-input-container flex> <label>City</label> <input ng-model="user.city"> </md-input-container> <md-input-container flex> <label>State</label> <input ng-model="user.state"> </md-input-container> <md-input-container flex> <label>Postal Code</label> <input ng-model="user.postalCode"> </md-input-container> </div> <md-input-container flex> <label>Biography</label> <textarea ng-model="user.biography" columns="1" md-maxlength="150"></textarea> </md-input-container> </form> </md-content> <div class="md-actions" layout="row"> <span flex></span> <md-button ng-click="answer(\'not useful\')"> Cancel </md-button> <md-button ng-click="answer(\'useful\')" class="md-primary"> Save </md-button> </div></md-dialog>',
+        targetEvent: ev,
+      })
+      .then(function(answer) {
+        $scope.alert = 'You said the information was "' + answer + '".';
+      }, function() {
+        $scope.alert = 'You cancelled the dialog.';
+      });
+  }
+
 }
+
+function DialogController($scope, $mdDialog) {
+  $scope.hide = function() {
+    $mdDialog.hide();
+  };
+  $scope.cancel = function() {
+    $mdDialog.cancel();
+  };
+  $scope.answer = function(answer) {
+    $mdDialog.hide(answer);
+  };
+};
