0732a24be4adeb2d9eb581a5f1bd76c56a8bb6bc
Alex <alexrules313@gmail.com>
2015-07-16 13:44:08 -0400

    login dialog box changes

--- tnw/app/components/app-nav/index.js
+++ tnw/app/components/app-nav/index.js
@@ -61,7 +61,59 @@ function controller ($scope, users, $state, $mdSidenav, $mdUtil, $log, $mdDialog
     $mdDialog.show({
         controller: controller,
         clickOutsideToClose: true,
-        template: '<md-dialog aria-label="popinLogin" class="loginPopup"> <div ng-if="setting"> <md-content class="md-padding"> <div class="pageTitle">Hi there!</div> <div> <a class="loginPopupSubheader" ui-sref="home" ng-click="SignUpDialog()"> New to Phrase? Sign Up</a> </div> <form name="Login" ng-submit="login(user)"> <div> <md-input-container flex class="loginPopupUsername"> <label>Username</label> <input type="text" ng-model="user.username"> </md-input-container> <md-input-container class="loginPopupPassword" flex> <label>Password</label> <input type="password" ng-model="user.password"> </md-input-container> </div> </form> </md-content> <div class="md-actions" layout="row"> <span flex></span> <md-button class="loginPopupCancel" ng-click="SignUpDialog()"> Sign Up </md-button> <md-button class="loginPopupLogin" type="button" ng-click="login(user)" class="md-raised md-primary"> Login </md-button> </div> </md-dialog> </div> <div ng-if="!setting"> <md-content class="md-padding"> <div class="pageTitle">Hi there!</div> <div> <a class="loginPopupSubheader" ui-sref="home" ng-click="LoginDialogTrue()"> Have an account? Login</a> </div> <form name="Signup" ng-submit="Signup(user)"> <div> <md-input-container flex class="loginPopupUsername"> <label>Username</label> <input type="text" ng-model="user.username"> </md-input-container> <md-input-container class="loginPopupPassword" flex> <label>Password</label> <input type="password" ng-model="user.password"> </md-input-container> </div> </form> </md-content> <div class="md-actions" layout="row"> <span flex></span> <md-button class="loginPopupCancel" ng-click="LoginDialogTrue()"> Login </md-button> <md-button class="loginPopupLogin" type="button" ng-click="signup(user)" class="md-raised md-primary"> Signup </md-button> </div> </div> </md-dialog>',
+        template: '<md-dialog aria-label="popinLogin" ng-class="{loginPopup: setting, signupPopup: !setting}">'+ 
+                    '<md-content class="md-padding">'+
+                      '<div class="pageTitle">Hi there!</div>'+
+                      '<div class="popupAnimate" ng-if="setting">'+
+                        '<div>'+
+                          '<a href class="loginPopupSubheader" ng-click="SignUpDialog()"> New to Phrase? Sign Up</a>'+
+                        '</div>'+
+                        '<form name="Login" ng-submit="loginButton(user)">'+
+                          '<div>'+
+                            '<md-input-container flex class="loginPopupUsername">'+
+                              '<label>Username</label>'+
+                              '<input type="text" ng-model="user.username">'+
+                            '</md-input-container>'+
+                            '<md-input-container class="loginPopupPassword" flex>'+
+                              '<label>Password</label>'+
+                              '<input type="password" ng-model="user.password">'+
+                            '</md-input-container>'+
+                          '</div>'+
+                          '<div class="md-actions" layout="row">'+
+                            '<span flex></span>'+
+                            '<md-button type="button" class="loginPopupCancel" ng-click="cancel()">'+
+                              '<span class="pageSubTitle">Cancel</span>'+
+                            '</md-button>'+
+                            '<md-button type="submit" class="loginPopupLogin md-raised md-primary" ng-click="loginButton(user)">Login</md-button>'+
+                          '</div>'+
+                        '</form>'+
+                      '</div>'+
+                      '<div ng-if="!setting">'+
+                        '<div>'+
+                          '<a class="loginPopupSubheader" href ng-click="LoginDialogTrue()"> Have an account? Login</a>'+
+                        '</div>'+
+                        '<form name="Signup" ng-submit="signupButton(user)" method="POST">'+
+                          '<div>'+
+                            '<md-input-container flex class="loginPopupUsername">'+
+                              '<label>Username</label>'+
+                              '<input type="text" name="username" ng-model="user.username">'+
+                            '</md-input-container>'+
+                            '<md-input-container class="loginPopupPassword" flex>'+
+                              '<label>Password</label>'+
+                              '<input type="password" name="pass" ng-model="user.password">'+
+                            '</md-input-container>'+
+                          '</div>'+
+                          '<div class="md-actions" layout="row">'+
+                            '<span flex></span>'+
+                            '<md-button type="button" class="loginPopupCancel" ng-click="cancel()">'+
+                              '<span class="pageSubTitle">Cancel</span>'+
+                            '</md-button>'+
+                            '<md-button type="submit" class="loginPopupLogin md-raised md-primary" ng-click="signupButtton(user)"> Signup </md-button>'+
+                          '</div>'+
+                        '</form>'+
+                      '</div>'+
+                    '</md-content>'+
+                  '</md-dialog>',
         targetEvent: ev,
       })
   }
@@ -74,32 +126,35 @@ function controller ($scope, users, $state, $mdSidenav, $mdUtil, $log, $mdDialog
     $scope.setting = false;
   }
 
-  $scope.login = function (user) {
+  $scope.loginButton = function (user) {
     $mdDialog.hide()
+    console.log("login")
     users.login(user.username, user.password)
       .then(function (res) {
         $rootScope.$broadcast('users.login.success')
-        $state.go('home')
       })
       .catch(function (err) {
         console.log(err)
       })
   }
 
-  $scope.signup = function (user) {
+  $scope.signupButton = function (user) {
+    console.log("signup")
     $mdDialog.hide()
     users.signup(user.username, user.password, user.email)
       .then(function (res) {
-        $state.go('users.login')
+        $state.go('articles.list')
       })
   };
   
   $scope.hide = function() {
     $mdDialog.hide();
+    console.log("hidden!")
   };
 
   $scope.cancel = function() {
     $mdDialog.cancel();
+    console.log("canceled!")
   };
 
   $scope.answer = function(answer) {
--- tnw/app/users/components/signup/index.js
+++ tnw/app/users/components/signup/index.js
@@ -4,11 +4,11 @@ var fs = require('fs')
 
 module.exports = {
   url: '/signup',
-  controller: ['$scope', 'users', '$state', '$rootScope', controller],
+  controller: ['$scope', 'users', '$state', '$stateParams', '$rootScope', controller],
   template: fs.readFileSync(__dirname + '/template.html', 'utf-8')
 }
 
-function controller ($scope, users, $state, $rootScope) {
+function controller ($scope, users, $state, $stateParams, $rootScope) {
   $scope.signup = function (user) {
     users.signup(user.username, user.password, user.email)
       .then(function (res) {
