e5d51f7f33dd8776a87d1d2e07f57461415136a8
David Golden <dcgolden1@gmail.com>
2015-07-20 15:57:30 -0400

    revert to annotator 2.x

--- tnw/app/articles/components/show/index.js
+++ tnw/app/articles/components/show/index.js
@@ -6,56 +6,36 @@ var $ = require('jquery');
 
 module.exports = {
     url: '/:id',
-    controller: ['$scope', 'articles', 'jquery', '$stateParams','$state', '$mdDialog', controller],
+    controller: ['$scope', 'articles', '$stateParams', '$state', '$mdDialog', controller],
     template: fs.readFileSync(__dirname + '/template.html', 'utf-8')
 };
 
-function controller($scope, articles, jQuery, $stateParams, $state, $mdDialog) {
+function controller($scope, articles, $stateParams, $state, $mdDialog) {
     articles.get($stateParams.id).then(function(doc) {
         $scope.article = doc;
     });
 
-    jQuery(function($) {
-            var content = $('#content').annotator();
-            content.annotator('addPlugin', 'Store', {
-                // The endpoint of the store on your server.
-                prefix: 'http://bivit-dev.iriscouch.com/annotator/_design/annotator/_rewrite',
+   
 
-                // Attach the uri of the current page to all annotations to allow search.
-                annotationData: {
-                    'uri': window.href
-                },
+    var remove = function(id) {
 
-                // This will perform a "search" action when the plugin loads. Will
-                // request the last 20 annotations for the current url.
-                // eg. /store/endpoint/search?limit=20&uri=http://this/document/only
-                loadFromSearch: {
-                    'limit': 20,
-                    'uri': window.href
-                }
-            });
+        articles.remove(id).then(function(res) {
+            $state.go('articles.list');
         });
+    };
 
-
-        var remove = function(id) {
-
-            articles.remove(id).then(function(res) {
-                $state.go('articles.list');
-            });
-        };
-
-        $scope.showConfirm = function(id) {
-            // Appending dialog to document.body to cover sidenav in docs app
-            var confirm = $mdDialog.confirm()
-                .parent(angular.element(document.body))
-                .title('Would you like to delete this article?')
-                .content('This will delete it forever.')
-                .ariaLabel('Article Delete')
-                .ok('Delete')
-                .cancel('Cancel');
-            $mdDialog.show(confirm).then(function() {
-                remove(id);
-                console.log('deleted!');
-            }, function() {});
-        };
-    }
+    $scope.showConfirm = function(id) {
+        // Appending dialog to document.body to cover sidenav in docs app
+        var confirm = $mdDialog.confirm()
+            .parent(angular.element(document.body))
+            .title('Would you like to delete this article?')
+            .content('This will delete it forever.')
+            .ariaLabel('Article Delete')
+            .ok('Delete')
+            .cancel('Cancel');
+        $mdDialog.show(confirm).then(function() {
+            remove(id);
+            console.log('deleted!');
+        }, function() {});
+    };
+}
