f78868c0ad8f9eca7d9ae5d97bfb99aac1d72921
David Golden <dcgolden1@gmail.com>
2015-07-29 14:52:45 -0400

    new server

--- tnw/app/index.js
+++ tnw/app/index.js
@@ -78,9 +78,9 @@ require('angular').module('app', [
     //our apps controller. we use one controller but it only 'loads' for whatever page we are on (also navbar and footer if they are in the view)
     .controller('AppController', ['$scope', 'classrooms', 'users', AppController])
     /*Constants for database name*/
-    .constant('dbName', 'https://phrasedev.cloudant.com/bivit')
-    .constant('remoteUserDbName', 'https://phrasedev.cloudant.com/_users')
-    .constant('annotationDbName', 'https://phrasedev.cloudant.com/annotator')
+    .constant('dbName', 'https://phrasedev.smileupps.com/phrase')
+    .constant('remoteUserDbName', 'https://phrasedev.smileupps.com/_users')
+    .constant('annotationDbName', 'https://phrasedev.smileupps.com/annotator')
     //.factory('classrooms', ['pouchDb', require('./services/classrooms')])
 
 //some functions we want everywhere in our app
